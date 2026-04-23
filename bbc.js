469af8f473f6f9f58f306c76ab3276511adb1382
airappandroid <aminurmarch@gmail.com>
2026-04-23 10:16:52 +0530

    Update bbc.js

--- bbc.js
+++ bbc.js
@@ -1,106 +1,109 @@
 window.SCHEDULE_DATA = [
-  { "time": "01:00 AM", "title": "Radio 1 Dance Drum & Bass Mix – DNB60 with Charlie Tee", "channel": "BBC Radio 1" },
-  { "time": "02:00 AM", "title": "Radio 1 Anthems – Stormzy, RAYE, Kings of Leon and more", "channel": "BBC Radio 1" },
-  { "time": "04:00 AM", "title": "Radio 1 Pop Anthems – Beabadoobee, Ed Sheeran, Sabrina Carpenter and more", "channel": "BBC Radio 1" },
+  { "time": "01:00 AM", "title": "Radio 1 Dance Presents – Three Six Zero: ALISHA", "channel": "BBC Radio 1" },
+  { "time": "02:00 AM", "title": "Radio 1 Anthems – Katy Perry, Bastille, Hozier & more", "channel": "BBC Radio 1" },
+  { "time": "04:00 AM", "title": "Radio 1 Pop Anthems – Troye Sivan, Ariana Grande & more", "channel": "BBC Radio 1" },
   { "time": "05:00 AM", "title": "Radio 1 Early Breakfast with Dean McCullough", "channel": "BBC Radio 1" },
 
-  { "time": "06:58 AM", "title": "Radio 1 Newsbeat – Latest news", "channel": "BBC Radio 1" },
-  { "time": "07:00 AM", "title": "Radio 1 Breakfast with Greg James", "channel": "BBC Radio 1" },
-  { "time": "10:30 AM", "title": "Radio 1 Newsbeat – Latest news", "channel": "BBC Radio 1" },
-  { "time": "10:33 AM", "title": "Rickie, Melvin and Charlie – Listener Dilemma", "channel": "BBC Radio 1" },
+  { "time": "06:58 AM", "title": "Radio 1 Newsbeat", "channel": "BBC Radio 1" },
+  { "time": "07:00 AM", "title": "Radio 1 Breakfast with Greg James – ThursWAYYY", "channel": "BBC Radio 1" },
+  { "time": "10:30 AM", "title": "Radio 1 Newsbeat", "channel": "BBC Radio 1" },
+  { "time": "10:33 AM", "title": "Rickie, Melvin and Charlie – Big hits & new music", "channel": "BBC Radio 1" },
 
-  { "time": "12:00 PM", "title": "Radio 1's Live Lounge – Holly Humberstone", "channel": "BBC Radio 1" },
-  { "time": "12:45 PM", "title": "Newsbeat – Latest news", "channel": "BBC Radio 1" },
-  { "time": "01:00 PM", "title": "Matt and Mollie – Mol’s Teaser", "channel": "BBC Radio 1" },
-  { "time": "03:30 PM", "title": "Radio 1 Newsbeat – Latest news", "channel": "BBC Radio 1" },
-  { "time": "03:33 PM", "title": "Going Home with Vick, Katie and Jamie – Midweek Movie Game", "channel": "BBC Radio 1" },
-  { "time": "05:45 PM", "title": "Newsbeat – Latest news", "channel": "BBC Radio 1" },
+  { "time": "12:00 PM", "title": "Radio 1 Live Lounge – Kacey Musgraves", "channel": "BBC Radio 1" },
+  { "time": "12:45 PM", "title": "Newsbeat", "channel": "BBC Radio 1" },
+  { "time": "01:00 PM", "title": "Matt and Mollie – The Five to Two Quiz", "channel": "BBC Radio 1" },
+  { "time": "03:00 PM", "title": "Radio 1 Pop Anthems with Matt and Lauren", "channel": "BBC Radio 1" },
+  { "time": "03:30 PM", "title": "Radio 1 Newsbeat", "channel": "BBC Radio 1" },
+  { "time": "03:33 PM", "title": "Going Home with Vick, Katie and Jamie", "channel": "BBC Radio 1" },
+  { "time": "05:45 PM", "title": "Newsbeat", "channel": "BBC Radio 1" },
 
-  { "time": "06:00 PM", "title": "Radio 1's New Music Show with Jack Saunders", "channel": "BBC Radio 1" },
-  { "time": "08:00 PM", "title": "Radio 1's Future Pop with Maia Beth", "channel": "BBC Radio 1" },
-  { "time": "10:00 PM", "title": "Radio 1's Power Down Playlist with Sian Eleri – Carefree Sounds", "channel": "BBC Radio 1" },
-  { "time": "11:00 PM", "title": "Benji B – Giving Prince his Flowers", "channel": "BBC Radio 1" },
-
-
-  { "time": "12:00 AM", "title": "OJ Borg – Punderland XI: Picnics", "channel": "BBC Radio 2" },
-  { "time": "03:00 AM", "title": "Sounds of the 90s with Fearne Cotton – 90s Running Songs Mixtape", "channel": "BBC Radio 2" },
-  { "time": "04:00 AM", "title": "Owain Wyn Evans – Early Breakfast: Tips and Thrifts", "channel": "BBC Radio 2" },
-
-  { "time": "06:30 AM", "title": "The Radio 2 Breakfast Show with Gary Davies and Sheridan Smith", "channel": "BBC Radio 2" },
-  { "time": "09:30 AM", "title": "Vernon Kay – Good News Wednesday", "channel": "BBC Radio 2" },
-
-  { "time": "12:00 PM", "title": "Jeremy Vine – News Discussion Show", "channel": "BBC Radio 2" },
-  { "time": "02:00 PM", "title": "Trevor Nelson – Midweek Music to Boost Your Mood", "channel": "BBC Radio 2" },
-  { "time": "04:00 PM", "title": "Sara Cox – Teatime Show", "channel": "BBC Radio 2" },
-
-  { "time": "06:30 PM", "title": "Sara Cox's Half Wower", "channel": "BBC Radio 2" },
-  { "time": "07:00 PM", "title": "Jo Whiley's Shiny Happy Playlist – Upbeat Tunes", "channel": "BBC Radio 2" },
-  { "time": "07:30 PM", "title": "Jo Whiley – Latest New Releases", "channel": "BBC Radio 2" },
-  { "time": "09:00 PM", "title": "The Folk Show with Mark Radcliffe", "channel": "BBC Radio 2" },
-  { "time": "10:00 PM", "title": "The Good Groove with DJ Spoony – Music is The Answer", "channel": "BBC Radio 2" },
-
- 
- 
-
-  { "time": "12:30 AM", "title": "Through the Night – Bach, Vasks, Britten and Silvestrov", "channel": "BBC Radio 3" },
-
-  { "time": "06:30 AM", "title": "Breakfast with Tom McKinney – Classical Music to Start the Day", "channel": "BBC Radio 3" },
-  { "time": "09:30 AM", "title": "Essential Classics with Georgia Mann", "channel": "BBC Radio 3" },
-
-  { "time": "01:00 PM", "title": "Classical Live – Once Upon A Time... Across Europe", "channel": "BBC Radio 3" },
-  { "time": "03:00 PM", "title": "Choral Evensong – Selwyn College, Cambridge", "channel": "BBC Radio 3" },
-  { "time": "04:00 PM", "title": "Composer of the Week – Joseph Haydn: Amadeus", "channel": "BBC Radio 3" },
-  { "time": "05:00 PM", "title": "In Tune – Eliane Correa and Las Salseras Perform Celia Cruz Music", "channel": "BBC Radio 3" },
-
-  { "time": "07:00 PM", "title": "Classical Mixtape – Inspirational Classical Music", "channel": "BBC Radio 3" },
-  { "time": "07:30 PM", "title": "Radio 3 in Concert – Dvořák, Kapustin and Ellington", "channel": "BBC Radio 3" },
-  { "time": "09:45 PM", "title": "The Essay – The Death and Life of Christopher Marlowe", "channel": "BBC Radio 3" },
-  { "time": "10:00 PM", "title": "Night Tracks with Sara Mohr-Pietsch – Late Night Soundscape", "channel": "BBC Radio 3" },
-  { "time": "11:30 PM", "title": "'Round Midnight – Joe Harriott Feature", "channel": "BBC Radio 3" },
+  { "time": "06:00 PM", "title": "New Music Show with Jack Saunders – Nemzzz ft. Lil Yachty", "channel": "BBC Radio 1" },
+  { "time": "08:00 PM", "title": "Radio 1 Rave-Up with Arielle Free", "channel": "BBC Radio 1" },
+  { "time": "10:00 PM", "title": "BBC Introducing Dance – Phoebe I-H", "channel": "BBC Radio 1" },
+  { "time": "11:00 PM", "title": "The Residency – Chris Stussy", "channel": "BBC Radio 1" },
 
 
   
-  { "time": "12:00 AM", "title": "Midnight News – National and International News", "channel": "BBC Radio 4" },
-  { "time": "12:30 AM", "title": "Self-Help from the Middle Ages – Pride", "channel": "BBC Radio 4" },
+  { "time": "12:00 AM", "title": "OJ Borg – Thursday means it’s nearly Friday", "channel": "BBC Radio 2" },
+  { "time": "03:00 AM", "title": "Alternative Sounds of the 90s with Dermot O'Leary – Dads Army!", "channel": "BBC Radio 2" },
+  { "time": "04:00 AM", "title": "Early Breakfast with Owain Wyn Evans – Airdrum Anthems", "channel": "BBC Radio 2" },
+
+  { "time": "06:30 AM", "title": "Radio 2 Breakfast Show with Gary Davies & Ralph Fiennes", "channel": "BBC Radio 2" },
+  { "time": "09:30 AM", "title": "Vernon Kay – What's in Vernon's Vault?", "channel": "BBC Radio 2" },
+
+  { "time": "12:00 PM", "title": "Jeremy Vine – News discussion", "channel": "BBC Radio 2" },
+  { "time": "02:00 PM", "title": "Trevor Nelson – Old-Skool Run", "channel": "BBC Radio 2" },
+  { "time": "04:00 PM", "title": "Sara Cox – Teatime show", "channel": "BBC Radio 2" },
+
+  { "time": "06:30 PM", "title": "Sara Cox's Half Wower – Dance tunes", "channel": "BBC Radio 2" },
+  { "time": "07:00 PM", "title": "Jo Whiley's Shiny Happy Playlist – Feel-good hits", "channel": "BBC Radio 2" },
+  { "time": "07:30 PM", "title": "Jo Whiley – Teeth 'n' Smiles session", "channel": "BBC Radio 2" },
+  { "time": "09:00 PM", "title": "The Country Show with Bob Harris – Featuring Darius Rucker", "channel": "BBC Radio 2" },
+  { "time": "10:00 PM", "title": "The Good Groove with DJ Spoony – Melvin Odoom mix", "channel": "BBC Radio 2" },
+ 
+ 
+
+
+  { "time": "12:30 AM", "title": "Through the Night – Debussy, Hindemith and Rameau from Oslo", "channel": "BBC Radio 3" },
+
+  { "time": "06:30 AM", "title": "Breakfast – Classical music and birdsong with Tom McKinney", "channel": "BBC Radio 3" },
+  { "time": "09:30 AM", "title": "Essential Classics – Classical favourites with Georgia Mann", "channel": "BBC Radio 3" },
+
+  { "time": "01:00 PM", "title": "Classical Live – Once Upon A Time... Music from Eastern Europe", "channel": "BBC Radio 3" },
+  { "time": "04:00 PM", "title": "Composer of the Week – Joseph Haydn: Spreading Fame", "channel": "BBC Radio 3" },
+  { "time": "05:00 PM", "title": "In Tune – Peter Mallinson performs A Musical Soirée", "channel": "BBC Radio 3" },
+
+  { "time": "07:00 PM", "title": "Classical Mixtape – Daily classical soundtrack", "channel": "BBC Radio 3" },
+  { "time": "07:30 PM", "title": "Radio 3 in Concert – Bartók: Bluebeard's Castle", "channel": "BBC Radio 3" },
+  { "time": "09:45 PM", "title": "The Essay – The Death and Life of Christopher Marlowe", "channel": "BBC Radio 3" },
+  { "time": "10:00 PM", "title": "Night Tracks – Immersive late-night music", "channel": "BBC Radio 3" },
+  { "time": "11:30 PM", "title": "'Round Midnight – New from Ruben Fox", "channel": "BBC Radio 3" },
+
+
+  
+  { "time": "12:00 AM", "title": "Midnight News", "channel": "BBC Radio 4" },
+  { "time": "12:30 AM", "title": "Self-Help from the Middle Ages – Anger", "channel": "BBC Radio 4" },
   { "time": "12:48 AM", "title": "Shipping Forecast", "channel": "BBC Radio 4" },
-  { "time": "01:00 AM", "title": "Selection of BBC World Service Programmes", "channel": "BBC Radio 4" },
+  { "time": "01:00 AM", "title": "BBC World Service Programmes", "channel": "BBC Radio 4" },
+
   { "time": "05:00 AM", "title": "News Summary", "channel": "BBC Radio 4" },
   { "time": "05:04 AM", "title": "Yesterday in Parliament", "channel": "BBC Radio 4" },
   { "time": "05:34 AM", "title": "Shipping Forecast", "channel": "BBC Radio 4" },
-  { "time": "05:43 AM", "title": "Prayer for the Day – Held in Loving Arms", "channel": "BBC Radio 4" },
-  { "time": "05:45 AM", "title": "Farming Today – Bird Flu Vaccine and Farming Issues", "channel": "BBC Radio 4" },
+  { "time": "05:43 AM", "title": "Prayer for the Day", "channel": "BBC Radio 4" },
+  { "time": "05:45 AM", "title": "Farming Today", "channel": "BBC Radio 4" },
 
-  { "time": "06:00 AM", "title": "Today – News and Current Affairs", "channel": "BBC Radio 4" },
-  { "time": "09:00 AM", "title": "Life Changing – I Fell 900ft and Survived", "channel": "BBC Radio 4" },
-  { "time": "09:30 AM", "title": "Currently – Inside ARIA", "channel": "BBC Radio 4" },
-  { "time": "10:00 AM", "title": "Woman's Hour – Topical Conversations", "channel": "BBC Radio 4" },
-  { "time": "11:00 AM", "title": "Today Debate – The Future of the Monarchy", "channel": "BBC Radio 4" },
-  { "time": "11:45 AM", "title": "Self-Help from the Middle Ages – Anger", "channel": "BBC Radio 4" },
+  { "time": "06:00 AM", "title": "Today – News & current affairs", "channel": "BBC Radio 4" },
+  { "time": "09:00 AM", "title": "In Our Time – Indian Indentured Labour", "channel": "BBC Radio 4" },
+  { "time": "09:45 AM", "title": "Strong Message Here – The Process Was Followed", "channel": "BBC Radio 4" },
+  { "time": "10:00 AM", "title": "Woman's Hour", "channel": "BBC Radio 4" },
+  { "time": "11:00 AM", "title": "This Cultural Life – Robert Icke", "channel": "BBC Radio 4" },
+  { "time": "11:45 AM", "title": "Self-Help from the Middle Ages – Gluttony", "channel": "BBC Radio 4" },
 
   { "time": "12:00 PM", "title": "News Summary", "channel": "BBC Radio 4" },
-  { "time": "12:04 PM", "title": "You and Yours – Consumer Affairs", "channel": "BBC Radio 4" },
-  { "time": "12:57 PM", "title": "Weather Forecast", "channel": "BBC Radio 4" },
-  { "time": "01:00 PM", "title": "World at One – News and Analysis", "channel": "BBC Radio 4" },
+  { "time": "12:04 PM", "title": "Scam Secrets", "channel": "BBC Radio 4" },
+  { "time": "12:32 PM", "title": "Sliced Bread – Hair Removal Devices", "channel": "BBC Radio 4" },
+  { "time": "12:57 PM", "title": "Weather", "channel": "BBC Radio 4" },
+  { "time": "01:00 PM", "title": "World at One", "channel": "BBC Radio 4" },
   { "time": "01:45 PM", "title": "Last Word – Doing Death Differently", "channel": "BBC Radio 4" },
   { "time": "02:00 PM", "title": "The Archers", "channel": "BBC Radio 4" },
-  { "time": "02:15 PM", "title": "Drama on 4 – Follow the Money", "channel": "BBC Radio 4" },
-  { "time": "03:00 PM", "title": "Money Box – Personal Finance News", "channel": "BBC Radio 4" },
-  { "time": "03:30 PM", "title": "Lab Rats", "channel": "BBC Radio 4" },
-  { "time": "04:00 PM", "title": "The Bottom Line – Richard Farleigh", "channel": "BBC Radio 4" },
-  { "time": "04:15 PM", "title": "The Media Show", "channel": "BBC Radio 4" },
-  { "time": "05:00 PM", "title": "PM – Full Coverage of the Day's News", "channel": "BBC Radio 4" },
+  { "time": "02:15 PM", "title": "Drama on 4 – King's Pawn", "channel": "BBC Radio 4" },
+  { "time": "03:00 PM", "title": "Open Country", "channel": "BBC Radio 4" },
+  { "time": "03:27 PM", "title": "Radio 4 Appeal – Magic Bus UK", "channel": "BBC Radio 4" },
+  { "time": "03:30 PM", "title": "Feedback", "channel": "BBC Radio 4" },
+  { "time": "04:00 PM", "title": "The Briefing Room", "channel": "BBC Radio 4" },
+  { "time": "04:30 PM", "title": "BBC Inside Science", "channel": "BBC Radio 4" },
+  { "time": "05:00 PM", "title": "PM – Evening news", "channel": "BBC Radio 4" },
 
   { "time": "06:00 PM", "title": "Six O'Clock News", "channel": "BBC Radio 4" },
-  { "time": "06:30 PM", "title": "Stand-Up Specials – Matt Hutchinson", "channel": "BBC Radio 4" },
+  { "time": "06:30 PM", "title": "Geoff Norcott's Working Men's Club", "channel": "BBC Radio 4" },
   { "time": "07:00 PM", "title": "The Archers", "channel": "BBC Radio 4" },
-  { "time": "07:15 PM", "title": "Front Row – Michael Jackson Biopic Review", "channel": "BBC Radio 4" },
-  { "time": "08:00 PM", "title": "AntiSocial – Super Skinny Celebs", "channel": "BBC Radio 4" },
-  { "time": "08:45 PM", "title": "Human Intelligence – Sei Shōnagon", "channel": "BBC Radio 4" },
-  { "time": "09:00 PM", "title": "The Life Scientific – Human-Robot Relations", "channel": "BBC Radio 4" },
-  { "time": "09:30 PM", "title": "Through Persian Eyes – Episode 3", "channel": "BBC Radio 4" },
+  { "time": "07:15 PM", "title": "Front Row", "channel": "BBC Radio 4" },
+  { "time": "08:00 PM", "title": "The Bottom Line – Richard Farleigh", "channel": "BBC Radio 4" },
+  { "time": "08:15 PM", "title": "The Media Show", "channel": "BBC Radio 4" },
+  { "time": "09:00 PM", "title": "Loose Ends", "channel": "BBC Radio 4" },
+  { "time": "09:45 PM", "title": "Strong Message Here", "channel": "BBC Radio 4" },
   { "time": "10:00 PM", "title": "The World Tonight", "channel": "BBC Radio 4" },
-  { "time": "10:45 PM", "title": "Few and Far Between – Episode 8", "channel": "BBC Radio 4" },
-  { "time": "11:00 PM", "title": "Brian & Roger – Old Habits", "channel": "BBC Radio 4" },
-  { "time": "11:15 PM", "title": "The Skewer – The Incredible Starmer", "channel": "BBC Radio 4" },
+  { "time": "10:45 PM", "title": "Few and Far Between", "channel": "BBC Radio 4" },
+  { "time": "11:00 PM", "title": "Radical with Amol Rajan", "channel": "BBC Radio 4" },
   { "time": "11:30 PM", "title": "Today in Parliament", "channel": "BBC Radio 4" }
 ];
